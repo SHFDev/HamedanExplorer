6394ce18be95a6cfa188192fa2287ab438fbb059
Sepehr <Sepehr@DESKTOP-QO1S20A>
2024-01-12 07:40:26 +0330

    .

--- Explorer/MVVM/ViewModel/HotelViewModel.cs
+++ Explorer/MVVM/ViewModel/HotelViewModel.cs
@@ -21,10 +21,10 @@ namespace Explorer.MVVM.ViewModel
             // افزودن رستوران‌ها به لیست
 
             Restaurants.Add(new Hotel_Model { Name = "هتل بوعلی همدان", Address = "همدان ، میدان جهاد ", Rating = 4.5, Star = "⭐⭐⭐⭐", about = "این هتل در تاریخ 1368 شمسی درست شده است", maploc = "https://neshan.org/maps/@34.784728,48.514918,15.9z,0p/places/9b0e28b66a4cd42738ef3467f924669b", profimage = "logoboali.png", titelimage = "h_boali.png" });
-            Restaurants.Add(new Hotel_Model { Name = "هتل بین المللی امیران همدان", Rating = 3.9, Star = "⭐⭐⭐⭐" });
-            Restaurants.Add(new Hotel_Model { Name = "هتل پارسیان همدان", Rating = 3.8, Star = "⭐⭐⭐⭐" });
-            Restaurants.Add(new Hotel_Model { Name = "هتل کتیبه همدان", Rating = 3.8, Star = "⭐⭐⭐⭐", Offering = 20, HasOffer = true });
-            Restaurants.Add(new Hotel_Model { Name = "هتل باباطاهر همدان", Rating = 4.8, Star = "⭐⭐⭐⭐⭐", Offering = 13, HasOffer = true });
+            Restaurants.Add(new Hotel_Model { Name = "هتل بین المللی امیران همدان", Rating = 3.9, Star = "⭐⭐⭐⭐", Address = "همدان پایین تر از پل فرهنگیان", about="هتل امیران در همدان یکی از بهترین هاب عهای بین المللی میباشد " ,HasOffer=true , Offering=10 ,maploc= "https://neshan.org/maps/@34.829923,48.525922,14.4z,0p/places/7826dd6cdb0bb2907725a7bca78eb810",profimage= "amiranlogo.png",titelimage= "amiran.jpeg" });
+            Restaurants.Add(new Hotel_Model { Name = "هتل پارسیان همدان", Rating = 3.8, Star = "⭐⭐⭐⭐",maploc= "https://neshan.org/maps/@34.781109,48.491191,14.5z,0p/places/44df30cb83f915e3518093c14c706f53",about="هتل پارسیان یکی از با سابفه ترین هتل ها در ایران و حتی همدان است",Address="همدان بلوار ، رو یه روی شهر بازی رنگین کمان" ,HasOffer=false ,profimage= "parsianlogo.png" , });
+            Restaurants.Add(new Hotel_Model { Name = "هتل کتیبه همدان", Rating = 3.8, Star = "⭐⭐⭐⭐", Offering = 20, HasOffer = true, maploc = "https://neshan.org/maps/@34.782656,48.485001,17.9z,0p/places/209c0d5f31d79a6fd3629509946ed574", about = " هتل کتیبه  یکی از شکیل ترین هتل ها یهمکدان محصوب می شود", Address = "همدان ، بلوار روبه روی بیمارستان بهشتی", profimage = "katibelogo.png", titelimage = "katibe.jpg" });
+            Restaurants.Add(new Hotel_Model { Name = "هتل باباطاهر همدان", Rating = 4.8, Star = "⭐⭐⭐⭐⭐", Offering = 13, HasOffer = true ,maploc= "https://neshan.org/maps/@34.810453,48.511412,17.2z,0p/places/0fa02e944cfa6c6d2e660d361662250d" ,about="یکی از بهترین هتل های پنج ستاره همدان هتل باباطاهر است" ,Address="همدان ، دور میدان باباطاهر" ,profimage= "babtaher.jpg",titelimage= "babtaher_img.png" });
         }
         public ICommand Res => new Command((p) =>
         {
--- Explorer/MVVM/ViewModel/ResturantViewModel.cs
+++ Explorer/MVVM/ViewModel/ResturantViewModel.cs
@@ -20,8 +20,8 @@ namespace Explorer.MVVM.ViewModel
         {
             // افزودن رستوران‌ها به لیست
 
-            Restaurants.Add(new Resturant_Model { Name = "رستوران عمارت", Rating = 4.5, Star = "⭐⭐⭐⭐",about= "این رستوران در زمان  های خیلی قدیم درست شد است خیلی خیلی قدیماین رستوران در زمان  های خیلی قدیم درست شد است خیلی خیلی قدیماین رستوران در زمان  های خیلی قدیم درست شد است خیلی خیلی قدیماین رستوران در زمان  های خیلی قدیم درست شد است خیلی خیلی قدیماین رستوران در زمان  های خیلی قدیم درست شد است خیلی خیلی قدیماین رستوران در زمان  های خیلی قدیم درست شد است خیلی خیلی قدیم ",Address="همدان خیابان مهدیه رو به روی عطا ویج" ,maploc= "https://neshan.org/maps/@34.790075,48.494531,18.1z,0p/places/600b924f7e4def6aa30d402ae20e3698" ,profimage="boali.png", titelimage="test.png"});
-            Restaurants.Add(new Resturant_Model { Name = "باغ رستوران نعل اشکنه", Rating = 3.8, Star = "⭐⭐⭐⭐" });
+            Restaurants.Add(new Resturant_Model { Name = "رستوران عمارت", Rating = 4.5, Star = "⭐⭐⭐⭐",about= "این رستوران در زمان های خیلی قدیم درست شذه  ",Address="همدان خیابان مهدیه رو به روی عطا ویج" ,maploc= "https://neshan.org/maps/@34.790075,48.494531,18.1z,0p/places/600b924f7e4def6aa30d402ae20e3698" ,profimage= "emaratlogo.png", titelimage= "emaratimg.png" });
+            Restaurants.Add(new Resturant_Model { Name = "باغ رستوران نعل اشکنه", Rating = 3.8, Star = "⭐⭐⭐⭐",Address="همدان ، بلوار کولاب",about="یکی از یهترین رستوران ها برای امتحان کردن مزه های خاص است" });
             Restaurants.Add(new Resturant_Model { Name = "فست فود عطاویچ", Rating = 3.8, Star = "⭐⭐⭐⭐" });
             Restaurants.Add(new Resturant_Model { Name = "سفره سرای آریاییان", Rating = 3.8, Star = "⭐⭐⭐⭐⭐" , Offering = 30 , HasOffer = true });
             Restaurants.Add(new Resturant_Model { Name = "فست فود رضا آقاجون", Rating = 3.8, Star = "⭐⭐⭐" , Offering = 10 , HasOffer = true});
