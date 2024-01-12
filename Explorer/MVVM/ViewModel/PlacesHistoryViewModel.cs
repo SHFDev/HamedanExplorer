470d697155ca7ead5ead6248b8db07b530afa32a
Sepehr <Sepehr@DESKTOP-QO1S20A>
2024-01-12 10:14:30 +0330

    .

--- Explorer/MVVM/ViewModel/PlacesHistoryViewModel.cs
+++ Explorer/MVVM/ViewModel/PlacesHistoryViewModel.cs
@@ -23,7 +23,7 @@ namespace Explorer.MVVM.ViewModel
                 Description = "\r\nزندگی و دستاوردها: ابوعلی حسین بن عبدالله بن سینا، معروف به بوعلی سینا و در غرب به ابن سینا، یکی از مشهورترین و تاثیرگذارترین فیلسوفان و دانشمندان دوران اسلامی است. او در حوزه‌های مختلفی چون پزشکی، فلسفه، ریاضیات و نجوم دستاوردهای مهمی داشته است.\r\nکتاب‌ها و آثار: از مهم‌ترین آثار او می‌توان به \"القانون فی الطب\" (قانون در طب) و \"کتاب الشفاء\" (کتاب شفا) اشاره کرد که در زمینه‌های پزشکی و فلسفه تأثیرات عمده‌ای داشته‌اند.",
                 imageSource = new List<string>()
                 {
-                    "bo_ali.jpg", "boali.jpg", "mainpic.jpg",
+                    "bo_ali.jpg", "boali.jpg", 
                 },
                 TitleImageSource = "bo_ali.jpg",
                 MapLocation = new Uri("https://neshan.org/maps/@34.791511,48.51393,17.2z,0p/places/75d0dbb1fa8d831d3c6f9f8b36354482"),
@@ -32,136 +32,93 @@ namespace Explorer.MVVM.ViewModel
             Places_History_Model history_Model1 = new Places_History_Model()
             {
                 Id = 1,
-                Name = "Test2",
-                Description = "test des2"
+                Name = "گنبد علویان",
+                Description = "\r\nیکی از علامت‌های شهر همدان است و در مرکز شهر واقع شده است. این بنا به عنوان یک گنبد آرامگاهی معروف است و در داخل آن، چندین گنبد و سردر وجود دارد",
+                TitleImageSource= "alavian.png",
+                imageSource = new List<string>()
+                {
+                    "alavian.png",
+                },
+                MapLocation = new Uri("https://neshan.org/maps/@34.803809,48.51416,15z,0p/places/b3c437ee561a3029b80697ebf43d908a"),
+
             };
            
             Places_History_Model history_Model2 = new Places_History_Model()
             {
                 Id = 1,
-                Name = "Test3",
-                Description = "test des3"
-            };
-            Places_History_Model history_Model3 = new Places_History_Model()
-            {
-                Id = 1,
-                Name = "Test4",
-                Description = "test des4",
-                imageSource = new List<string>()
+                Name = "گنج نامه",
+                Description = "",
+                TitleImageSource= "ganjname.png",
+                
+                 imageSource = new List<string>()
                 {
-                    "mainpic.jpg", "mainpic.jpg",  "mainpic.jpg",
+                    "g1.jpg",  "g2.jpg",
                 },
                 MapLocation = new Uri("https://neshan.org/maps/@34.798402,48.515585,17.3z,0p/places/f4bf373cc75e64458ba760aa08420102"),
 
             };
-            Places_History_Model history_Model4 = new Places_History_Model()
-            {
-                Id = 1,
-                Name = "Test5",
-                Description = "test des5"
-            };
+          
             History.Add(history_Model);
             History.Add(history_Model1);
             History.Add(history_Model2);
-            History.Add(history_Model3);
-            History.Add(history_Model4);
+           
 
             Places_History_Model Religious_Model = new Places_History_Model()
             {
                 Id = 1,
-                Name = "آرامگاه بوعلی سینا",
-                Description = "\r\nزندگی و دستاوردها: ابوعلی حسین بن عبدالله بن سینا، معروف به بوعلی سینا و در غرب به ابن سینا، یکی از مشهورترین و تاثیرگذارترین فیلسوفان و دانشمندان دوران اسلامی است. او در حوزه‌های مختلفی چون پزشکی، فلسفه، ریاضیات و نجوم دستاوردهای مهمی داشته است.\r\nکتاب‌ها و آثار: از مهم‌ترین آثار او می‌توان به \"القانون فی الطب\" (قانون در طب) و \"کتاب الشفاء\" (کتاب شفا) اشاره کرد که در زمینه‌های پزشکی و فلسفه تأثیرات عمده‌ای داشته‌اند.",
+                Name = "امازاده عبدالله",
+                Description = "امامزاده عبدالله یکی از مشهورترین بقاع متبرکه در همدان است. این بنا متعلق به دوره سلجوقی است و دارای گنبدی زیبا و کاشی‌کاری‌های منحصربه‌فرد است",
                 imageSource = new List<string>()
                 {
-                    "bo_ali.jpg", "boali.jpg", "mainpic.jpg",
+                    "em1.png", "em.png", 
                 },
-                TitleImageSource = "bo_ali.jpg",
-                MapLocation = new Uri("https://neshan.org/maps/@34.791511,48.51393,17.2z,0p/places/75d0dbb1fa8d831d3c6f9f8b36354482"),
-            };
-            Places_History_Model Religious_Model1 = new Places_History_Model()
-            {
-                Id = 1,
-                Name = "Test2",
-                Description = "test des2"
-            };
-            Places_History_Model Religious_Model2 = new Places_History_Model()
-            {
-                Id = 1,
-                Name = "Test3",
-                Description = "test des3"
-            };
-            Places_History_Model Religious_Model3 = new Places_History_Model()
-            {
-                Id = 1,
-                Name = "Test4",
-                Description = "test des4",
-                imageSource = new List<string>()
-                {
-                    "mainpic.jpg", "mainpic.jpg",  "mainpic.jpg",
-                },
-                MapLocation = new Uri("https://neshan.org/maps/@34.798402,48.515585,17.3z,0p/places/f4bf373cc75e64458ba760aa08420102"),
-
+                TitleImageSource = "em1.jpg",
+                MapLocation = new Uri("https://neshan.org/maps/@34.802641,48.509378,16.3z,0p/places/32e98ce134daae5a6e806b9c4107d610"),
             };
             Places_History_Model Religious_Model4 = new Places_History_Model()
             {
                 Id = 1,
-                Name = "Test5",
-                Description = "test des5"
+                Name = "امامزاده یحیی",
+                Description = "این امامزاده نیز از جمله مکان‌های مقدس و مورد احترام در همدان است که برای زائران و علاقه‌مندان به اماکن مذهبی اهمیت دارد",
+                 imageSource = new List<string>()
+                {
+                    "eah1.jpg", "eah12.jpg", 
+                },
+                TitleImageSource = "eah1.jpg",
+                MapLocation = new Uri("https://neshan.org/maps/@34.793174,48.520018,18.7z,0p/places/616797e8c31adb9692fe4441494d1c11"),
             };
             Religious.Add(Religious_Model);
-            Religious.Add(Religious_Model1);
-            Religious.Add(Religious_Model2);
-            Religious.Add(Religious_Model3);
             Religious.Add(Religious_Model4); 
             
-            Places_History_Model Explorer_Model = new Places_History_Model()
-            {
-                Id = 1,
-                Name = "test2",
-                Description = "\r\nزندگی و دستاوردها: ابوعلی حسین بن عبدالله بن سینا، معروف به بوعلی سینا و در غرب به ابن سینا، یکی از مشهورترین و تاثیرگذارترین فیلسوفان و دانشمندان دوران اسلامی است. او در حوزه‌های مختلفی چون پزشکی، فلسفه، ریاضیات و نجوم دستاوردهای مهمی داشته است.\r\nکتاب‌ها و آثار: از مهم‌ترین آثار او می‌توان به \"القانون فی الطب\" (قانون در طب) و \"کتاب الشفاء\" (کتاب شفا) اشاره کرد که در زمینه‌های پزشکی و فلسفه تأثیرات عمده‌ای داشته‌اند.",
-                imageSource = new List<string>()
-                {
-                    "bo_ali.jpg", "boali.jpg", "mainpic.jpg",
-                },
-                TitleImageSource = "bo_ali.jpg",
-                MapLocation = new Uri("https://neshan.org/maps/@34.791511,48.51393,17.2z,0p/places/75d0dbb1fa8d831d3c6f9f8b36354482"),
-            };
+           
             Places_History_Model Explorer_Model1 = new Places_History_Model()
             {
+
                 Id = 1,
-                Name = "hghdfddfh",
-                Description = "test des2"
-            };
-            Places_History_Model Explorer_Model2 = new Places_History_Model()
-            {
-                Id = 1,
-                Name = "dfgh",
-                Description = "test des3"
+                Name = "پارک مردم همدان",
+                Description = "\r\nاین پارک یکی از بزرگ‌ترین و زیباترین پارک‌های همدان است که فضایی دلپذیر برای پیاده‌روی، استراحت و تفریح خانوادگی فراهم می‌کند.",
+                TitleImageSource="park1.png",
+                 imageSource = new List<string>()
+                {
+                    "park1.jpg", "park2.jpg", "park3.jpg"
+                },
+                MapLocation = new Uri("https://neshan.org/maps/@34.802641,48.509239,16.5z,0p/places/32e98ce134daae5a6e806b9c4107d610"),
             };
             Places_History_Model Explorer_Model3 = new Places_History_Model()
             {
                 Id = 1,
-                Name = "dfgdfhdfhdfh",
-                Description = "test dfhdfhdfhdfhdfhdfhdfhdfhdfhdfhdfh",
+                Name = "کوه الوند",
+                Description = "کوهستان الوند با مناظر طبیعی خیره‌کننده، مکانی عالی برای کوهنوردی، طبیعت‌گردی و تفریحات فضای باز است. این کوهستان فرصت‌های عالی برای عکاسی و لذت بردن از مناظر طبیعی فراهم می‌کند.",
                 imageSource = new List<string>()
                 {
-                    "mainpic.jpg", "mainpic.jpg",  "mainpic.jpg",
+                    "al1.png", "al2.png", "al3.png",
                 },
                 MapLocation = new Uri("https://neshan.org/maps/@34.798402,48.515585,17.3z,0p/places/f4bf373cc75e64458ba760aa08420102"),
+                TitleImageSource = "al1.png",
+            };
 
-            };
-            Places_History_Model Explorer_Model4 = new Places_History_Model()
-            {
-                Id = 1,
-                Name = "sdfgbdfhb",
-                Description = "test des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5\ndes5des5des5des5des5des5des5des5des5\ndes5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5des5\ndes5des5des5des5des5des5des5des5",
-                TitleImageSource = "bo_ali.jpg",
-            };
-            Explorer.Add(Explorer_Model);
             Explorer.Add(Explorer_Model1);
-            Explorer.Add(Explorer_Model2);
             Explorer.Add(Explorer_Model3);
-            Explorer.Add(Explorer_Model4);
         }
     }
 }
